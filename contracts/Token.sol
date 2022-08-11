a4f16b6c2ee128bed14fcd09f8b0964df10b1f50
MadDog250 <101847688+MadDog250@users.noreply.github.com>
2022-08-11 09:27:11 -0500

    Update Token.sol

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.6;
 
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.0.0/contracts/token/ERC20/ERC20.sol";
 
-contract MyToken is ERC20 {
+contract Token is ERC20 {
     constructor(string memory name, string memory symbol) ERC20(name, symbol) {
         // Mint 100 tokens to msg.sender
         // Similar to how
