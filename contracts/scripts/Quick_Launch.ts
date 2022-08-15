fb2bb87dcf2795f006ca66b2adbc32ef25659237
MadDog250 <101847688+MadDog250@users.noreply.github.com>
2022-08-15 17:36:13 -0500

    Update Quick_Launch.ts

--- contracts/scripts/Quick_Launch.ts
+++ contracts/scripts/Quick_Launch.ts
@@ -1,12 +1,12 @@
 // This script can be used to launch a basic MTOTM using Juicebox Contracts already deployed to Rinkeby.
-// Please make sure to compile "JBController, ERC20Terminal, and Token" files before running this script.
+// Please make sure to compile "./Token.sol, ./ERC20Terminal, and ./JBController" file before running this script.
 // And use Right click -> "Run" from context menu of the file to run the script. Shortcut: Ctrl+Shift+S
 
 import { deploy } from './launch'
 import { launch } from './launch'
+import {owner} from './launch'
 
 
-let owner = 'YOUR_ADDRESS';
 var Token;
 var Terminal;
 let Operator    = '0xEDB2db4b82A4D4956C3B4aA474F7ddf3Ac73c5AB';
@@ -19,15 +19,19 @@ let SingleTokenStore = '0xb814D3d1a33449830018eC945e98ae03532A8a1f';
 
 (async () => {
     try {
+        
         const result = await deploy('Token', ['TEST', "T"])
+        console.log(`address: ${result.address}`)
         Token = result.address
         const terminal = await deploy('JBERC20PaymentTerminal', [Token,1,2,2,Operator,Projects,Directory,SplitsStore,Prices,SingleTokenStore,owner])
+        console.log(`address: ${terminal.address}`)
         Terminal = terminal.address
         const project = await launch('JBController', [owner,['QmbH96jj8RTJgC9526RdsFs5dPvcsRfiRuD9797JXzcvbw', 0],
-        [0,1000000000000000,0,"0x0000000000000000000000000000000000000000"],[[false,false],0,10000,0,false,false,false,false,false,false,true,false,false,false,false,false,"0x0000000000000000000000000000000000000000"]
+        [0,1000000000000000,0,"0x0000000000000000000000000000000000000000"],[[false,false],0,10000,0,false,false,false,false,false,false,true,false,false,false,false,false,"0x0000000000000000000000000000000000000000"],
         0,[],[],["0x765A8b9a23F58Db6c8849315C04ACf32b2D55cF8", Terminal],'yes',])
-        console.log(`project hash: ${project.hash}`)
+        console.log(`hash: ${project.hash}`)
     } catch (e) {
         console.log(e.message)
     }
   })()
+
