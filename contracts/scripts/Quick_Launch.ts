98024e2b37be288501c63d2a08ee289c766f1f9e
MadDog250 <101847688+MadDog250@users.noreply.github.com>
2022-09-01 11:51:32 -0500

    Update Quick_Launch.ts

--- contracts/scripts/Quick_Launch.ts
+++ contracts/scripts/Quick_Launch.ts
@@ -1,16 +1,16 @@
-// This script can be used to deploy the "Storage" contract using ethers.js library.
+// This script can be used to deploy the "Token" and "ERC20Terminal" contracts while launching a MTOTM project using ethers.js library.
 // Please make sure to compile "./Token.sol, ./ERC20Terminal, and ./JBController" file before running this script.
 // And use Right click -> "Run" from context menu of the file to run the script. Shortcut: Ctrl+Shift+S
 
 import { deploy } from './launch'
 import { launch } from './launch'
 import {owner} from './launch'
-//import {amount} from './launch'
-//import { approve } from './launch'
 
 export let Token;
 //var Token;
 var Terminal;
+let ETHTerminal = '0x765A8b9a23F58Db6c8849315C04ACf32b2D55cF8';
+let ETH         = '0x000000000000000000000000000000000000EEEe';
 let Operator    = '0xEDB2db4b82A4D4956C3B4aA474F7ddf3Ac73c5AB';
 let Projects    = '0x2d8e361f8F1B5daF33fDb2C99971b33503E60EEE';
 let Directory   = '0x1A9b04A9617ba5C9b7EBfF9668C30F41db6fC21a';
@@ -22,26 +22,28 @@ let SingleTokenStore = '0xb814D3d1a33449830018eC945e98ae03532A8a1f';
 (async () => {
     try {
         
-        const result = await deploy('Token', ['TEST', "T"])
+        const result = await deploy('Token', ['Mike', "T"])
         console.log(`address: ${result.address}`)
         Token = result.address
         const terminal = await deploy('JBERC20PaymentTerminal', [Token,1,2,1,Operator,Projects,Directory,SplitsStore,Prices,SingleTokenStore,owner])
         console.log(`address: ${terminal.address}`)
         Terminal = terminal.address
         const project = await launch('JBController', [owner,['QmbH96jj8RTJgC9526RdsFs5dPvcsRfiRuD9797JXzcvbw', 0], //IPFS Hash
-        [0,1000000000000000,0,"0x0000000000000000000000000000000000000000"], //Token amount per ETH
-        [[false,false],0,8000,0,false,false,false,false,false,false,true,false,false,false,false,false,"0x0000000000000000000000000000000000000000"], //Funding cycle metadata (reserve amt, redemption amt)
-        0, //Timestamp
-        [
-        [1, [[false,false,1000000000,0,"0x6eD8B3d76c3ffAE797029f8fB2f7DBd34b6Bd9eC",0,"0x0000000000000000000000000000000000000000"]]],  
-        [2, [[false,false,1000000000,0,"0x6eD8B3d76c3ffAE797029f8fB2f7DBd34b6Bd9eC",0,"0x0000000000000000000000000000000000000000"]]]
-        ], //Splits
-        [[Terminal,Token,BigInt(1000000000000000000000000000),1,0,0]], //Provides routing from terminal to split number provided in terminal deploy
-        ["0x765A8b9a23F58Db6c8849315C04ACf32b2D55cF8",Terminal],'yes',]) //Terminals
+            [0,BigInt(1000000000000000000000000),0,"0x0000000000000000000000000000000000000000"], //Token amount per ETH
+            [[false,false],2000,0,0,false,false,false,false,false,false,true,false,false,false,false,false,"0x0000000000000000000000000000000000000000"], //Funding cycle metadata (reserve amt, redemption amt)
+             0, //Timestamp
+            [
+                [1, [[false,false,1000000000,0,"0x6eD8B3d76c3ffAE797029f8fB2f7DBd34b6Bd9eC",0,"0x0000000000000000000000000000000000000000"]]],  
+                [2, [[false,false,1000000000,0,"0x6eD8B3d76c3ffAE797029f8fB2f7DBd34b6Bd9eC",0,"0x0000000000000000000000000000000000000000"]]]
+            ], //Splits
+            [
+                [ETHTerminal,ETH,BigInt(100000000000000000000000000000),1,0,0],
+                [Terminal,Token,BigInt(100000000000000000000000000000),1,0,0]
+            ], //Provides routing from terminal to split number provided in terminal deploy
+            [ETHTerminal,Terminal],'',]  //Terminals
+         ) 
         console.log(`hash: ${project.hash}`)
 
-       // const approval = await approve('Token', [Terminal, amount])
-        // console.log(`hash: ${approval.hash}`)
     } catch (e) {
         console.log(e.message)
     }
